b2522887c7cf785f940a0292ad84e0e92df8b13a
Mark Vayngrib <mark@tradle.io>
2016-10-23 22:58:47 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,238 @@
+'use strict'
+
+/** @module contextsDB */
+
+const EventEmitter = require('events').EventEmitter
+const debug = require('debug')('tradle:dbs:context')
+const pump = require('pump')
+const through = require('through2')
+const indexer = require('feed-indexer')
+const lexint = require('lexicographic-integer')
+const tradle = require('@tradle/engine')
+const topics = tradle.topics
+const types = tradle.types
+const utils = tradle.utils
+const typeforce = tradle.typeforce
+const constants = tradle.constants
+const SEQ = constants.SEQ
+const TYPE = constants.TYPE
+const MESSAGE_TYPE = constants.MESSAGE_TYPE
+const ENTRY_PROP = constants.ENTRY_PROP
+
+/**
+ * @typedef {Object} contextsDB
+ */
+
+/**
+ * blockchain seals database, bootstrapped from log
+ *
+ * @alias module:contextsDB
+ * @param  {Object}   opts
+ * @param  {Object}   opts.node            @tradle/engine node instance
+ * @param  {Object}   opts.db              database to use to track message context
+ * @param  {Function} [opts.getMessageSeq] calculate the seq of a message object (defaults to msg => msg[SEQ])
+ * @param  {Function} [opts.getContext]    calculate the context of a message object (defaults to msg => msg.context)
+ */
+module.exports = function createContextDB (opts) {
+  typeforce({
+    node: typeforce.Object,
+    db: typeforce.String,
+    // pass these in to override the defaults
+    getMessageSeq: typeforce.maybe(typeforce.Function),
+    getContext: typeforce.maybe(typeforce.Function)
+  }, opts)
+
+  const node = opts.node
+  const keeper = node.keeper
+  const customTopics = {
+    sharecontext: 'sharectx',
+    unsharecontext: 'unsharectx'
+  }
+
+  let closed
+  const msgDB = node._createDB('msg-' + opts.db)
+  msgDB.once('closing', () => closed = true)
+
+  const ctxDB = node._createDB('ctx-' + opts.db)
+  ctxDB.once('closing', () => closed = true)
+
+  node.once('destroying', () => {
+    msgDB.close()
+    ctxDB.close()
+  })
+
+  const getMessageSeq = opts.getMessageSeq || defaultGetMessageSeq
+  const getContext = opts.getContext || defaultGetContext
+  const indexedMsgDB = indexer({
+    feed: node.changes,
+    db: msgDB,
+    primaryKey: 'permalink',
+    entryProp: ENTRY_PROP,
+    preprocess: preprocess,
+    reduce: function (state, change, cb) {
+      const val = change.value
+      const context = getContext(val)
+      if (!context) return cb()
+
+      let newState
+      switch (val.topic) {
+      case topics.newobj:
+        return cb(null, {
+          permalink: val.permalink,
+          context: context,
+          recipient: getRecipient(val),
+          seq: getMessageSeq(val) // not the same as message.seq
+        })
+      }
+
+      cb()
+    }
+  })
+
+  const msgDBIndexes = {
+    context: indexedMsgDB.by('context', function (state) {
+      // order by seq
+      return state.context + sep + lexint.pack(state.seq) + sep + state.permalink
+    })
+  }
+
+  const indexedCtxDB = indexer({
+    feed: node.changes,
+    db: ctxDB,
+    primaryKey: value => `${getContext(value)}:${getRecipient(value)}`,
+    entryProp: ENTRY_PROP,
+    filter: function (val) {
+      return val.topic === customTopics.sharecontext || val.topic === customTopics.unsharecontext
+    },
+    reduce: function (state, change, cb) {
+      const val = change.value
+      const context = getContext(val)
+      if (!context) return cb()
+
+      let newState
+      switch (val.topic) {
+      case customTopics.sharecontext:
+        newState = state ? clone(state) : newContextState(val)
+        newState.active = true
+        break
+      case customTopics.unsharecontext:
+        if (!state) return cb() // was never shared
+
+        newState = clone(state)
+        newState.active = false
+        break
+      }
+
+      cb(null, newState)
+    }
+  })
+
+  const myDebug = utils.subdebugger(debug, node.name || node.shortlink)
+  const sep = indexedCtxDB.separator
+  const indexes = {}
+  // indexes.recipient = indexedCtxDB.by('recipient')
+  // indexes.active = indexedCtxDB.by('active')
+  indexes.context = indexedCtxDB.by('context', function (state) {
+    if (!state.context) return
+    return state.context + sep + lexint.pack(state.seq)
+  })
+
+  indexes.contextForRecipient = indexedCtxDB.by('cfr', function (state) {
+    if (!state.context) return
+    return state.active + sep + state.context + sep + state.recipient // + sep + lexint.pack(state.seq)
+  })
+
+  const forwarding = {}
+  pump(
+    indexes.contextForRecipient.createReadStream({ live: true, keys: false }),
+    through.obj(function (data, enc, cb) {
+      forward(data)
+      cb()
+    })
+  )
+
+  return {
+    share: function share ({ context, recipient, seq=0 }, cb) {
+      node.changes.append({
+        topic: 'sharectx',
+        timestamp: utils.now(),
+        context: context,
+        recipient: recipient,
+        seq: seq
+      }, cb)
+    },
+    unshare: function unshare ({ context, recipient }, cb) {
+      node.changes.append({
+        topic: 'unsharectx',
+        timestamp: utils.now(),
+        context: context,
+        recipient: recipient
+      }, cb)
+    }
+  }
+
+  function forward (data) {
+    const identifier = data.context + data.recipient
+    if (forwarding[identifier]) return
+
+    const to = { permalink: data.recipient }
+    forwarding[identifier] = pump(
+      getMessageStream(data),
+      through.obj(function (data, enc, cb) {
+        keeper.get(data.permalink, function (err, object) {
+          if (err) {
+            myDebug(`data not found for message: ${data.permalink}`)
+            return cb()
+          }
+
+          node.send({ object, to }, cb)
+        })
+      })
+    )
+  }
+
+  function getMessageStream (data) {
+    return msgDBIndexes.context.createReadStream({
+      gte: data.context + sep + lexint.pack(data.seq),
+      lte: data.context + sep + '\xff',
+      old: true,
+      live: true,
+      keys: false
+    })
+  }
+
+  function preprocess (change, cb) {
+    if (closed) return
+
+    const val = change.value
+    const topic = val.topic
+    if (val.type !== MESSAGE_TYPE || topic !== topics.newobj) return cb()
+
+    keeper.get(val.permalink, function (err, body) {
+      if (err) return cb(null, change)
+
+      val.object = body
+      cb(null, change)
+    })
+  }
+}
+
+function getRecipient (val) {
+  return val.recipient
+}
+
+function newContextState (val) {
+  return {
+    context: val.context,
+    recipient: val.recipient,
+    seq: 0
+  }
+}
+
+function defaultGetContext (val) {
+  return val.context || val.object.context
+}
+
+function defaultGetMessageSeq (msg, context) {
+  return msg.object[SEQ]
+}
---
+++ test.js
@@ -0,0 +1,112 @@
+'use strict'
+
+const test = require('tape')
+const tradle = require('@tradle/engine')
+const helpers = require('@tradle/engine/test/helpers')
+const contexts = require('@tradle/engine/test/contexts')
+const createContextsDB = require('./')
+const constants = tradle.constants
+const TYPE = constants.TYPE
+
+test('contexts', function (t) {
+  contexts.nFriends(3, function (err, friends) {
+    if (err) throw err
+
+    const contextDBs = friends.map(node => {
+      return createContextsDB({
+        node: node,
+        db: 'contexts.db'
+      })
+    })
+
+    const [alice, bob, carol] = friends
+    helpers.connect(friends)
+
+    let link
+    let context = 'boo!'
+    const msg1 = {
+      [TYPE]: 'something',
+      hey: 'ho'
+    }
+
+    const msg2 = {
+      [TYPE]: 'something else',
+      hey: 'hey'
+    }
+
+    let msg1wrapper
+    let msg2wrapper
+    alice.signAndSend({
+      to: bob._recipientOpts,
+      object: msg1,
+      other: {
+        context: context
+      }
+    }, rethrow)
+
+    bob.once('message', msg => {
+      msg1wrapper = msg
+
+      // bob
+      // test share existing
+      contextDBs[1].share({
+        context,
+        recipient: carol.permalink,
+        seq: 0
+      })
+    })
+
+    // no context
+    bob.signAndSend({
+      to: alice._recipientOpts,
+      object: {
+        [TYPE]: 'something',
+        hey: 'ho'
+      }
+    }, rethrow)
+
+    let togo = 2
+    carol.once('message', msg => {
+      // if (msg.author !== bob.permalink) return
+
+      // msg = {
+      //   ...metadata,
+      //   object: {
+      //     [TYPE]: 'tradle.Message',  // msg from alice
+      //     object: {
+      //       [TYPE]: 'tradle.Message' // original msg from alice
+      //       object: {}               // msg1 body
+      //     }
+      //   }
+      // }
+
+      t.equal(msg.objectinfo.link, msg1wrapper.link)
+      t.equal(msg.object.object.context, context)
+
+      // test share live
+      alice.signAndSend({
+        to: bob._recipientOpts,
+        object: msg2,
+        other: {
+          context: context
+        }
+      }, function (err, result) {
+        if (err) throw err
+
+        msg2wrapper = result.message
+      })
+
+      carol.once('message', msg => {
+        // if (msg.author !== alice.permalink) return
+
+        t.same(msg.object.object, msg2wrapper.object)
+        t.end()
+        friends.forEach(f => f.destroy())
+      })
+    })
+  })
+})
+
+function rethrow (err) {
+  if (err) throw err
+}
